aac8d4c4cf063b30ad22bff12f5436edcce93e52
Julien Ricard <julien.ricard@gmail.com>
2016-11-22 18:08:05 +0100

    Initial commit.

---
+++ click_detector.py
@@ -0,0 +1,252 @@
+"""
+Basic click detection. Ermites 2016.
+"""
+
+import logging
+import argparse
+import numpy as np
+import copy
+from collections import defaultdict, OrderedDict
+
+import soundfile as sf
+from scipy.signal import butter, lfilter, fftconvolve
+
+
+def build_butter_bandpass(lowcut, highcut, sr, order=5):
+    nyq = 0.5 * sr
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='bandpass')
+    return b, a
+
+def butter_filter(data, filter):
+    return lfilter(filter[0], filter[1], data)
+
+def get_envelope(data, sr, env_sr):
+
+    # full-wave rectification
+    data = np.abs(data)
+
+    # half-hanning smoothing
+    win_duration = 0.05 # in s
+    win_size = win_duration * sr
+    win = np.hanning(win_size)
+    win_max_ind = np.argmax(win)
+    win = win[win_max_ind:] / np.sum(win) * 2
+
+    # downsampling
+    downsampling_ratio = int(sr / env_sr)
+    return np.log(fftconvolve(data, win, 'same'))[::downsampling_ratio]
+
+def clean_detections(detection_dict, clean_size):
+    "Remove detected values if a higher one is found within clean_size samples"
+    prev_k = -1
+    prev_v = -1
+    to_remove = set()
+    for k, v in detection_dict.items():
+        if k - prev_k < clean_size:
+            if v > prev_v:
+                to_remove.add(prev_k)
+                prev_k = k
+                prev_v = v
+            else:
+                to_remove.add(k)
+        else:
+            prev_k = k
+            prev_v = v
+    
+    for k in to_remove:
+        try:
+            del detection_dict[k]
+        except:
+            pass # only when k = -1
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    parser = argparse.ArgumentParser(description="""Click detector based on onset
+        detection.\nA click is detected if an onset is detected in all the frequency
+        bands.""")
+    parser.add_argument(
+        '-v', "--verbose",
+        help="Set verbose output", action="store_const", const=logging.DEBUG,
+        dest="loglevel", default=logging.INFO)
+    parser.add_argument("input", help="Audio file.")
+    parser.add_argument("output", help="Output csv file with detections.")
+    parser.add_argument("--show", type=int, default=0, help="""Plot audio, clicks
+            and some more stuff.""")
+    parser.add_argument("--antares", type=int, default=0, help="""Reject clicks with
+        onsets in antares bands (check acutofffreqs in the code).""")
+    parser.add_argument("--channel", type=int, default=0, help="Audio channel to process")
+    args = parser.parse_args()
+
+    logging.getLogger().setLevel(args.loglevel)
+
+    input = args.input
+    output = args.output
+    show = args.show
+    antares = args.antares
+    channel = args.channel
+
+    # open audio file
+    audio, sr = sf.read(input)
+
+    if len(audio.shape) > 1:
+        audio = audio[:, channel]
+
+    # compute downsampling ratio
+    env_sr = 1000
+    downsampling_ratio = int(sr / env_sr)
+
+    # build N bandpass filters for whale detection
+    wcutofffreqs = [(11000, 12000), (9000, 10000), (8000, 9000), (7000, 8000),
+            (6000, 7000)] # keep bands in decreasing frequency
+    whale_filters = []
+    for f in wcutofffreqs:
+        whale_filters.append(build_butter_bandpass(f[0], f[1], sr))
+
+    if antares:
+        # build M bandpass filters to detect antares clicks
+        acutofffreqs = [(60000, 80000)] # keep bands in decreasing frequency
+        rejection_filters = []
+        for f in acutofffreqs:
+            rejection_filters.append(build_butter_bandpass(f[0], f[1], sr))
+
+    # filter signal
+    whale_bands = []
+    for f in whale_filters:
+        whale_bands.append(butter_filter(audio, f))
+    if antares:
+        rejection_bands = []
+        for f in rejection_filters:
+            rejection_bands.append(butter_filter(audio, f))
+
+    # get log envelopes
+    whale_envelopes = []
+    for s in whale_bands:
+        whale_envelopes.append(get_envelope(s, sr, env_sr))
+    if antares:
+        rejection_envelopes = []
+        for s in rejection_bands:
+            rejection_envelopes.append(get_envelope(s, sr, env_sr))
+
+    # get derivative
+    whale_derivatives = []
+    for s in whale_envelopes:
+        whale_derivatives.append(np.diff(s))
+    if antares:
+        rejection_derivatives = []
+        for s in rejection_envelopes:
+            rejection_derivatives.append(np.diff(s))
+    
+    # get values above a threshold
+    threshold = 0.2
+    whale_detections = []
+    for s in whale_derivatives:
+        d = defaultdict(int)
+        for i in np.where(s>threshold)[0]:
+            d[i] += s[i]
+        whale_detections.append(OrderedDict(sorted(d.items())))
+    if antares:
+        rejection_detections = []
+        for s in rejection_derivatives:
+            d = defaultdict(int)
+            for i in np.where(s>threshold)[0]:
+                d[i] += s[i]
+            rejection_detections.append(OrderedDict(sorted(d.items())))
+
+    clicks = copy.deepcopy(whale_detections[0])
+
+    # keep the highest values in a 100ms window (only in highest band = the
+    # reference band)
+    clean_size = int(0.05 * env_sr)
+    clean_detections(clicks, clean_size)
+
+    # frequency integration: keep only clicks detected in all whale bands
+    # get clicks in the highest band and check if some click exists in other
+    # bands between -50ms and +50ms
+    to_remove = set()
+    for k0 in clicks.keys():
+        for d in whale_detections[1:]:
+            found = False
+            for k in d.keys():
+                if np.abs(k0 - k) < clean_size:
+                    found = True
+                    break
+            if not found:
+                to_remove.add(k0)
+                break
+
+    for k in to_remove:
+        del clicks[k]
+
+    if antares:
+        # now if a click is found in rejection bands, remove it
+        to_remove = set()
+        for k0 in clicks.keys():
+            for d in rejection_detections:
+                found = False
+                for k in d.keys():
+                    if np.abs(k0 - k) < clean_size:
+                        found = True
+                        break
+                if found:
+                    to_remove.add(k0)
+                    break
+
+        for k in to_remove:
+            del clicks[k]
+
+    with open(output, "w") as f:
+        for k, v in clicks.items():
+            f.write("{:.3f},{:.3f}\n".format(k / env_sr, v))
+
+    if show:
+        # plot stuff
+        x = np.arange(len(audio)) / sr
+
+        
+        fig = plt.figure()
+        
+        ax0 = fig.add_subplot(411)
+        ax0.plot(x, audio, 'b')
+        ax0.set_title("Full band: Audio signal + final clicks")
+        ax1 = ax0.twinx()
+        t = np.asarray(list(clicks.keys())) / env_sr
+        c = np.asarray(list(clicks.values()))
+        ax1.scatter(t, c, marker="x", c="r")
+        ax1.set_xlim(left=0)
+
+        ax2 = fig.add_subplot(412)
+        ax2.plot(x, whale_bands[0], 'g')
+        ax2.set_title("Detection band 1: Audio signal + derivatives + detections)")
+        ax3 = ax2.twinx()
+        ax3.plot(x[::downsampling_ratio][:-1], whale_derivatives[0], 'r')
+        t = np.asarray(list(whale_detections[0].keys())) / env_sr
+        c = np.asarray(list(whale_detections[0].values()))
+        ax3.scatter(t, c, marker="x")
+        ax3.set_xlim(left=0)
+        
+        ax4 = fig.add_subplot(413)
+        ax4.plot(x, whale_bands[1], 'g')
+        ax4.set_title("Detection band 2: Audio signal + derivatives + detections)")
+        ax5 = ax4.twinx()
+        ax5.plot(x[::downsampling_ratio][:-1], whale_derivatives[1], 'r')
+        t = np.asarray(list(whale_detections[1].keys())) / env_sr
+        c = np.asarray(list(whale_detections[1].values()))
+        ax5.scatter(t, c, marker="x")
+        ax5.set_xlim(left=0)
+        
+        if antares:
+            ax6 = fig.add_subplot(414)
+            ax6.plot(x, rejection_bands[0], 'g')
+            ax6.set_title("Retection band 1: Audio signal + derivatives + detections)")
+            ax7 = ax6.twinx()
+            ax7.plot(x[::downsampling_ratio][:-1], rejection_derivatives[0], 'r')
+            t = np.asarray(list(rejection_detections[0].keys())) / env_sr
+            c = np.asarray(list(rejection_detections[0].values()))
+            ax7.scatter(t, c, marker="x")
+            ax7.set_xlim(left=0)
+
+        plt.show()
---
+++ xchannel_spermwhale_click_delay.py
@@ -0,0 +1,140 @@
+"""        
+2-channels Inter Pulse Interval detection and delay measure.
+"""
+
+import os
+import logging
+import argparse
+from argparse import RawDescriptionHelpFormatter
+import textwrap
+
+import soundfile as sf
+import numpy as np
+from scipy.signal import butter, lfilter, fftconvolve
+
+
+def build_butter_highpass(cut, sr, order=5):
+    nyq = 0.5 * sr
+    cut = cut / nyq
+    b, a = butter(order, cut, btype='highpass')
+    return b, a
+
+def butter_filter(data, filter):
+    return lfilter(filter[0], filter[1], data)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description=textwrap.dedent('''
+    2-channels Inter Pulse Interval detection and delay measure.
+    The algorithm is the following:
+    For every click in the click file, in a 0.1s window around the click: 
+        - Filter both channels with highpass filter with 8000 Hz cutoff frequency.
+        - In channel 2, check if the autocorrelation of the click has a peak in the [ipi_min, ipi_max]
+          range (in second) with amplitude higher than a threshold
+        - If yes compute the cross-correlation between channels 1 and 2. The position of the peak gives the delay between both channels.
+          Write output to click_root.
+
+    The file written, if any, will be in click_root, have the same basename as the audio file and end with "delays.csv".
+    The format of each line is:
+    
+    <date>, <time>, <click time>, <click confidence>, <xcorr delay>, <acorr ipi delay>, <acorr value at 0 delay>, <acorr value at ipi delay>
+    '''), formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument(
+        '-v', "--verbose",
+        help="Set verbose output", action="store_const", const=logging.DEBUG,
+        dest="loglevel", default=logging.INFO)
+    parser.add_argument("input", help="Audio file.")
+    parser.add_argument("click_root", help="Click file root directory.")
+    parser.add_argument("--ipi_min", type=float, default=0.0015, help="Minimum IPI to be detected in s.")
+    parser.add_argument("--ipi_max", type=float, default=0.008, help="Maximum IPI to be detected in s.")
+    parser.add_argument("--filter_cutoff", type=int, default=8000, help="""Cut-off frequency of the high-pass filter in Hz.""")
+    parser.add_argument("--swap_channels", type=int, default=0, help="""Swap channels 1 and 2 in algorithm.""")
+    args = parser.parse_args()
+
+    logging.getLogger().setLevel(args.loglevel)
+
+    input = args.input
+    click_root = args.click_root
+    ipi_min = args.ipi_min
+    ipi_max = args.ipi_max
+    swap_channels = args.swap_channels
+    filter_cutoff = args.filter_cutoff
+
+    #############################
+    # open and parse click file #
+    #############################
+
+    click_path = os.path.join(click_root, os.path.basename(input) + ".csv")
+    
+    with open(click_path, "r") as f:
+        clicks = []
+        for line in f.readlines():
+            split_line = line.split(",")
+            clicks.append((float(split_line[0]), float(split_line[1])))
+
+    ###################   
+    # open audio file #
+    ###################   
+
+    audio, sr = sf.read(input)
+
+    if not len(audio.shape) == 2:
+        raise Exception("File must have 2 channels")
+
+    ###########
+    # process #
+    ###########
+
+    delays = []
+
+    for c, v in clicks:
+
+        # Get bandpassed audio in 100ms around the detected clicks and compute
+        # autocorrelation in channel 1
+
+        filter = build_butter_highpass(filter_cutoff, sr)
+
+        win_start_ind = int((c - 0.05) * sr)
+        win_end_ind = int((c + 0.05) * sr)
+
+        ch1 = butter_filter(audio[win_start_ind:win_end_ind, 1 if swap_channels else 0],
+                filter)
+        ch2 = butter_filter(audio[win_start_ind:win_end_ind, 0 if swap_channels else 1],
+                filter)
+
+        # Compute autocorrelation of ch2
+        ch2_acorr = np.abs(np.correlate(ch2, ch2, "same"))
+
+        # Compute ratio between highest value in the [ipi_min,ipi_max] autocorrelation
+        # range and the autocorrelation value at 0 delay
+
+        ac_d0 = np.argmax(ch2_acorr) # max at 0
+        ac_v0 = ch2_acorr[ac_d0]
+
+        ac_win_start = int(ac_d0 + ipi_min * sr)
+        ac_win_end = int(ac_d0 + ipi_max * sr)
+        peak_ind = ac_win_start + np.argmax(ch2_acorr[ac_win_start:ac_win_end])
+        peak_value = ch2_acorr[peak_ind]
+
+        ratio = ac_v0 / peak_value
+
+        # If the ratio is smaller than a threshold, get intercorrelation peak
+        threshold = 10 # arbitrary threshold to be investigated some day
+        if ratio < threshold:
+            xcorr = np.abs(np.correlate(ch1, ch2, "same"))
+            xc_delay = (np.argmax(xcorr) - ac_d0) / sr
+            ipi = (peak_ind - ac_d0) / sr
+            delays.append((c, v, xc_delay, ipi, ac_v0, peak_value))
+
+    if delays:
+
+        # parse date / time from filename
+        filename = os.path.basename(input)
+        date = filename.split("_")[0]
+        time = filename.split("_")[1]
+
+        with open(click_path.replace("csv", "delays.csv"), "w") as f:
+            for c, v, xc_delay, ipi, ac_v0, peak_value in delays:
+                f.write("{},{},{},{},{},{},{:.5f},{:.5f}\n".format(
+                    date,time, c, v, xc_delay, ipi, ac_v0, peak_value))
