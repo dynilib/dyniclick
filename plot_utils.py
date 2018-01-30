763079bbcae5b8e1095721382e4cf71b33f989f4
Julien Ricard <julien.ricard@gmail.com>
2018-01-30 15:12:44 +0100

    Add track data

--- plot_utils.py
+++ plot_utils.py
@@ -13,19 +13,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_data(click_file, data, feat_names, feat_thres, time_offset):
+def plot_data(click_file, data, feat_names, feat_thres, time_offset, track_file):
     
     f, axarr = plt.subplots(len(feat_names), sharex=True)
 
     title = click_file if time_offset == 0.0 else "{}\ntime offset: {}".format(click_file, time_offset)
     axarr[0].set_title(title)
 
-    t = np.asarray([d[0] for d in data]) + time_offset
+
+    # map track_ind to color if tracks are set
+    colorlist = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    colors = [colorlist[int(d%(len(colorlist)-1))] for d in data[:,-1]] if track_file else 'b'
+    
+    t = np.asarray([d[0] for d in data])
 
     for i in range(len(feat_names)):
 
         d = np.asarray([d[i+1] for d in data])
-        axarr[i].scatter(t, d, marker="x", c="b")
+        axarr[i].scatter(t, d, marker="x", c=colors)
         ymax = np.max(data[:,i+1])
         ymax = ymax + np.abs(ymax) / 10
         ymin = np.min(data[:,i+1])
@@ -55,6 +60,7 @@ if __name__ == "__main__":
     parser.add_argument("--feat_scale", type=float, nargs="+", default=[],
                         help="Feature scaling factor.")
     parser.add_argument("--time_offset", type=float, default=0.0, help="Time offset.")
+    parser.add_argument("--track_file", type=str, default='', help="Track file.")
 
     args = parser.parse_args()
 
@@ -64,6 +70,7 @@ if __name__ == "__main__":
     feat_thres = args.feat_thres
     feat_scale = args.feat_scale
     time_offset = args.time_offset
+    track_file = args.track_file
 
 
     if (len(feat_names) != len(feat_cols) or
@@ -76,6 +83,12 @@ if __name__ == "__main__":
     data = np.loadtxt(click_file, delimiter=',')
     feat_cols = [0] + feat_cols # col 0 is click time
     data = data[:,feat_cols]
+
+    # if track_file is set, add track ind to data
+    if track_file:
+        track_ind = np.loadtxt(track_file, ndmin=2)
+        data = np.hstack((data, track_ind))
+
     for i, t in enumerate(feat_thres):
         if t != 0:
             data = data[data[:,i+1]>t]
@@ -85,4 +98,4 @@ if __name__ == "__main__":
     if data.size == 0:
         raise Exception("No data")
     
-    plot_data(click_file, data, feat_names, feat_thres, time_offset)
+    plot_data(click_file, data, feat_names, feat_thres, time_offset, track_file)
